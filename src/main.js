b2e95d084f943f3035b4b8570ddeba4bba505477
LazyNeko1 <48885326+LazyNeko1@users.noreply.github.com>
2020-01-15 12:30:03 -0800

    started?

--- src/main.js
+++ src/main.js
@@ -1,3 +1,36 @@
-const nekoTotal = "http://neko-bot.net/info/totalnekos.txt"
-const animeTotal = "http://neko-bot.net/info/totalanime.txt"
+const nekoTotal = "http://neko-bot.net/info/totalnekos.txt";
+const animeTotal = "http://neko-bot.net/info/totalanime.txt";
 
+class random {
+  
+  neko(min, max, source) {
+    var total = fetch(nekoTotal) // get total images
+    ID = Math.floor((Math.random() * total) + 1); // calculate ID of image
+    if (source = true) {
+      out = {"url":url, "source":source, "id":ID} //output as json (?)
+    }
+    //if source is not true, output normally. (eg if its a string/0)    
+    if (source != true){
+      out = "http://neko-bot.net/nekos/" + ID + ".png"
+    }
+    return out
+  };
+  
+  
+  
+  
+  
+  anime(min, max, source) {
+    var total = fetch(nekoTotal) // get total images
+    ID = Math.floor((Math.random() * total) + 1); // calculate ID of image
+    if (source = true){
+      out = {"url":url, "source":source, "id":ID} //output as json (?)
+    }
+    //if source is not true, output normally. (eg if its a string/0)
+    if (source != true){
+      out = "http://neko-bot.net/anime/"+ID+".png"
+    }
+    return out
+  };  
+  
+}
